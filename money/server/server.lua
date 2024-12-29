38292ee6c1c0101f30c631ab48fcee168119c2dc
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2024-12-28 23:05:15 -0800

    Update server.lua

--- money/server/server.lua
+++ money/server/server.lua
@@ -1,106 +1,123 @@
+-- Ensure the resource name is correct
 if (GetCurrentResourceName() ~= "money") then
     print("[^1DEBUG^0] Please make sure the resource name is ^3money^0 or else exports won't work.")
 end
 
 local accounts = {}
 
-RegisterNetEvent('NAT2K15:CHECKSQL')
-AddEventHandler('NAT2K15:CHECKSQL', function(steam, discord, first_name, last_name, twt, dept, dob, gender, data) 
-    local src = source
-    if(accounts[src]) then
-        MySQL.Async.execute("UPDATE money SET bank = @bank, amount = @amount WHERE steam = @steam AND first = @first AND last = @last AND dept = @dept", {["@bank"] = accounts[src].bank, ["@amount"] = accounts[src].amount, ["@steam"] = steam, ["@first"] = accounts[src].first, ["@last"] = accounts[src].last, ["@dept"] = accounts[src].dept})
-        
+-- Function to add money
+function addMoney(id, amount)
+    if accounts[id] then
+        accounts[id].amount = accounts[id].amount + amount
+        TriggerClientEvent('NAT2K15:UPDATEPAY', id, accounts[id])
     end
-    MySQL.Async.fetchAll("SELECT * FROM money WHERE steam = @steam AND first = @first AND last = @last AND dept = @dept", {["@steam"] = steam, ["@first"] = first_name, ["@last"] = last_name, ["@dept"] = dept}, function(data) 
-        if(data[1] == nil) then
-            MySQL.Async.execute("INSERT INTO money (steam, discord, first, last, dept, bank) VALUES (@steam, @discord, @first, @last, @dept, @bank)", {["@steam"] = steam, ["@discord"] = discord, ["@first"] = first_name, ["@last"] = last_name,  ["@dept"] = dept, ["@bank"] = config.starting_money })
-            accounts[src] = {steam = steam, discord = discord, amount = 0, bank = tonumber(config.starting_money), cycle = config.deptPay[dept], dept = dept, first = first_name, last = last_name}
-            TriggerClientEvent('NAT2K15:UPDATECLIENTMONEY', src, accounts[src])
-        else 
-            accounts[src] = {steam = steam, discord = discord, amount = tonumber(data[1].amount), bank = tonumber(data[1].bank), cycle = config.deptPay[dept], dept = dept, first = first_name, last = last_name}
-            TriggerClientEvent('NAT2K15:UPDATECLIENTMONEY', src, accounts[src])
+end
+
+-- Function to deduct money
+function deductMoney(id, amount)
+    if accounts[id] and accounts[id].amount >= amount then
+        accounts[id].amount = accounts[id].amount - amount
+        TriggerClientEvent('NAT2K15:UPDATEPAY', id, accounts[id])
+        return true
+    else
+        return false -- Not enough money
+    end
+end
+
+-- Function to deposit money into the bank
+function depositMoney(id, amount)
+    if accounts[id] and accounts[id].amount >= amount then
+        accounts[id].amount = accounts[id].amount - amount
+        accounts[id].bank = accounts[id].bank + amount
+        TriggerClientEvent('NAT2K15:UPDATEPAY', id, accounts[id])
+    else
+        TriggerClientEvent('NAT2K15:BANKNOTIFY', id, "Insufficient cash to deposit.")
+    end
+end
+
+-- Function to withdraw money from the bank
+function withdrawMoney(id, amount)
+    if accounts[id] and accounts[id].bank >= amount then
+        accounts[id].bank = accounts[id].bank - amount
+        accounts[id].amount = accounts[id].amount + amount
+        TriggerClientEvent('NAT2K15:UPDATEPAY', id, accounts[id])
+    else
+        TriggerClientEvent('NAT2K15:BANKNOTIFY', id, "Insufficient funds in the bank.")
+    end
+end
+
+-- Event to check or create account in the database
+RegisterNetEvent('NAT2K15:CHECKSQL')
+AddEventHandler('NAT2K15:CHECKSQL', function(steam, discord, first_name, last_name, dept)
+    local src = source
+    if accounts[src] then
+        MySQL.Async.execute("UPDATE money SET bank = @bank, amount = @amount WHERE steam = @steam", {
+            ["@bank"] = accounts[src].bank,
+            ["@amount"] = accounts[src].amount,
+            ["@steam"] = steam
+        })
+    end
+
+    MySQL.Async.fetchAll("SELECT * FROM money WHERE steam = @steam", { ["@steam"] = steam }, function(data)
+        if data[1] == nil then
+            MySQL.Async.execute("INSERT INTO money (steam, discord, first, last, dept, bank, amount) VALUES (@steam, @discord, @first, @last, @dept, @bank, @amount)", {
+                ["@steam"] = steam,
+                ["@discord"] = discord,
+                ["@first"] = first_name,
+                ["@last"] = last_name,
+                ["@dept"] = dept,
+                ["@bank"] = config.starting_money,
+                ["@amount"] = 0
+            })
+            accounts[src] = { steam = steam, discord = discord, bank = config.starting_money, amount = 0, dept = dept }
+        else
+            accounts[src] = { steam = steam, discord = discord, bank = tonumber(data[1].bank), amount = tonumber(data[1].amount), dept = dept }
         end
+        TriggerClientEvent('NAT2K15:UPDATECLIENTMONEY', src, accounts[src])
     end)
 end)
 
--- if(config.enable_change_your_own_cycle) then
---     RegisterCommand('setsalary', function(source, args, message) 
---         local length = args[1];
---         if(length == nil) then return TriggerClientEvent('chatMessage', source, "[^3SYSTEM^0] Please ensure to have a valid amount.") end
---         length = tonumber(length)
---         if(length == nil) then return TriggerClientEvent('chatMessage', source, "[^3SYSTEM^0] Please ensure to have a valid amount.") end
---         if(length > 99999) then length = 99999 end
---         if(length < 0) then length = config.deptPay[accounts[source].dept] end
---         TriggerClientEvent('chatMessage', source, "[^3BANK^0] Your daily salary has been changed to ^2" .. length)
---         if(accounts[source]) then
---             accounts[source].cycle = length
---             TriggerClientEvent('NAT2K15:UPDATECLIENTMONEY', source, accounts[source])
---         end
---     end)
--- end
-
-AddEventHandler('playerDropped', function(reason) 
+-- Save account data when a player leaves
+AddEventHandler('playerDropped', function()
     local src = source
-    if(accounts[src]) then 
-        MySQL.Async.fetchAll("SELECT * FROM money WHERE steam = @steam AND first = @first AND last = @last AND dept = @dept", {["@steam"] = accounts[src].steam, ["@first"] =  accounts[src].first, ["@last"] =  accounts[src].last, ["@dept"] =  accounts[src].dept}, function(data) 
-            if(data[1] ~= nil) then
-                MySQL.Async.execute("UPDATE money SET amount = @amount, bank = @bank WHERE steam = @steam AND first = @first AND last = @last AND dept = @dept", {["@steam"] = accounts[src].steam, ["@amount"] = tonumber(accounts[src].amount), ["@bank"] = tonumber(accounts[src].bank), ["@first"] =  accounts[src].first, ["@last"] =  accounts[src].last, ["@dept"] =  accounts[src].dept})
-                print("^3[Money Saving Account]^0 Saving " .. accounts[src].discord .. " Steam: " .. accounts[src].discord .. " Name: " .. accounts[src].first .. " " .. accounts[src].last .. "Bank: " .. accounts[src].bank .. " Cash: " .. accounts[src].amount)
-            end
-        end)
+    if accounts[src] then
+        MySQL.Async.execute("UPDATE money SET bank = @bank, amount = @amount WHERE steam = @steam", {
+            ["@bank"] = accounts[src].bank,
+            ["@amount"] = accounts[src].amount,
+            ["@steam"] = accounts[src].steam
+        })
+        accounts[src] = nil
     end
 end)
 
-Citizen.CreateThread(function() 
+-- Pay salary periodically
+Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(config.cycle_length * 1000 * 60) -- config.cycle_length * 1000 * 60
+        Citizen.Wait(config.cycle_length * 60 * 1000)
         for _, player in ipairs(GetPlayers()) do
-            player = tonumber(player)
-            local src = player
-            if(accounts[player]) then
-                if(accounts[player].dept) then
-                    accounts[player].bank =  config.deptPay[accounts[player].dept] + tonumber(accounts[player].bank)
-                    TriggerClientEvent('NAT2K15:UPDATEPAYCHECK', player, player, accounts[player])
-                end
+            local src = tonumber(player)
+            if accounts[src] and accounts[src].dept then
+                accounts[src].bank = accounts[src].bank + config.deptPay[accounts[src].dept]
+                TriggerClientEvent('NAT2K15:UPDATEPAY', src, accounts[src])
             end
         end
     end
 end)
 
-exports('getaccount', function(id) 
-    if(accounts[id]) then
-        return accounts[id]
-    else    
-        return nil
-    end
+-- Exports for external usage
+exports('addMoney', addMoney)
+exports('deductMoney', deductMoney)
+exports('depositMoney', depositMoney)
+exports('withdrawMoney', withdrawMoney)
+
+exports('getaccount', function(id)
+    return accounts[id]
 end)
 
-exports('updateaccount', function(id, array) 
-    if(accounts[id]) then
-        accounts[id].amount = array.cash
-        accounts[id].bank = array.bank
-        TriggerClientEvent('NAT2K15:UPDATEPAY', id, id, accounts[id])
-        return true
-    else 
-        return nil
+exports('updateaccount', function(id, data)
+    if accounts[id] then
+        accounts[id].amount = data.amount
+        accounts[id].bank = data.bank
+        TriggerClientEvent('NAT2K15:UPDATEPAY', id, accounts[id])
     end
-end)
-
-exports('sendmoney', function(id, sendid, idarray, sendidarray) 
-    if(accounts[id]) then
-        accounts[id].amount = idarray.cash
-        accounts[id].bank = idarray.bank
-        TriggerClientEvent('NAT2K15:UPDATEPAY', id, id, accounts[id])
-    end
-
-    if(accounts[sendid]) then
-        accounts[sendid].amount = sendidarray.cash
-        accounts[sendid].bank = sendidarray.bank
-        TriggerClientEvent('NAT2K15:UPDATEPAY', sendid, id, accounts[sendid])
-    end
-end)
-
-
-exports('bankNotify', function(id, message) 
-    TriggerClientEvent('NAT2K15:BANKNOTIFY', id, message)
 end)
